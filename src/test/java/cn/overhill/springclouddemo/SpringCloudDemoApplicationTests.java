d57b4065bba47c7b94d0ccafa9036007b0c8a8a7
wumu <2232728687@qq.com>
2021-01-24 14:41:49 +0800

    feat: 初次提交

---
+++ src/test/java/cn/overhill/springclouddemo/SpringCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.overhill.springclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
