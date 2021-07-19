5ece1f4f530dbf0ca397c42037b2d7deff7c7080
wumu <2232728687@qq.com>
2021-07-19 21:34:55 +0800

    feat: 添加websocket

--- demo-user/src/main/java/cn/overhill/user/UserApplicaton.java
+++ demo-user/src/main/java/cn/overhill/user/UserApplicaton.java
@@ -4,9 +4,8 @@ package cn.overhill.user;
 import org.apache.catalina.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class UserApplicaton {
     public static void main(String[] args){
